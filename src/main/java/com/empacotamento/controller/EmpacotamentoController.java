9022108de4ac14b42897859e689632c92293e19a
Mauro Telles <msergiost@hotmail.com>
2025-10-20 15:41:32 -0300

    Adicionado versão no controller

--- src/main/java/com/empacotamento/controller/EmpacotamentoController.java
+++ src/main/java/com/empacotamento/controller/EmpacotamentoController.java
@@ -1,6 +1,7 @@
 package com.empacotamento.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,11 @@ public class EmpacotamentoController implements EmpacotamentoApi {
 		PedidosSaidaWrapper pedidosSaidaWrapper = new ProcessarPedidosEntrada().processar(pedidosWrapper);
 
 		return ResponseEntity.ok(pedidosSaidaWrapper);
+	}
+	
+	@GetMapping
+	public String getVersion() {
+		return "1.1.1";
 	}
 
 }
